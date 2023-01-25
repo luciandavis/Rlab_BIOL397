26fadc0eb8b7158e6f81cab188defe8f1cd97cf6
luciandavis <luciandavis@u.boisestate.edu>
2023-01-25 14:50:19 -0700

    First commit

---
+++ week2.R
@@ -0,0 +1,18 @@
+library(tidyverse)
+library(dplyr)
+library(devtools)
+install.packages("knitr")
+library(knitr)
+install.packages("bookdown")
+library(bookdown)
+install.packages("rmarkdown")
+library(rmarkdown)
+3+9
+#this is a basic equation
+2+2 #this is a basic equation
+this is a basic equation
+library(usethis)
+use_git_config(user.name = "luciandavis", user.email = "luciandavis@u.boisestate.edu")
+usethis::create_github_token()
+#finally connected with Github
+2+2
